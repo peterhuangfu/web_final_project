5fd0e21053fcb82b070bfd0dabe47485c23b3fec
peterhuangfu <nightlight870213@gmail.com>
2019-06-13 14:29:46 +0800

    front-end initialize

---
+++ front-end/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import HashRouter from 'react-router-dom';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <HashRouter>
+				<div className="homepage">
+				</div>
+			</HashRouter>
+    );
+  }
+}
+
+export default App;
