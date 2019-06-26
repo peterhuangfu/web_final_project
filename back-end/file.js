0f635e10e8920409646ab2db33de7c8dd7970109
oomtball <b05705051@ntu.edu.tw>
2019-06-26 14:21:25 +0800

    add getFile
    add getFile

---
+++ back-end/file.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// this will be our data base's data structure 
+const DataSchema = new Schema(
+  { 
+    upload_time: String,
+    file_title: String,  
+    file_name: String,  
+    file_description: String,  
+    user_name: String,  
+  }
+);
+
+// export the new Schema so we could modify it using Node.js
+module.exports = mongoose.model("file", DataSchema);
